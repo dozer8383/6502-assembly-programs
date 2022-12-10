0709c17c1d38835ba05eca796ddd003a8176cfeb
Dozer8383 <119268610+dozer8383@users.noreply.github.com>
2022-12-10 14:07:49 -0600

    Update repeat.asm

--- repeat.asm
+++ repeat.asm
@@ -1,5 +1,10 @@
+;a small template to make any code loop (of course, as long as it doesn't interfere with the X register).
+;Simply change the value of CPX to however many times you want it to loop.
+
 repeat:
-;do stuff
-DEC $00
-CPX #$01
+;insert code here
+INX
+CPX #$06
 BEQ repeat
+
+;for an infinite loop, remove INX (still making sure that the X register is not being used by your code)
